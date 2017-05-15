35edf545abb377e7b3e4dada78e4a0b6e7d4126f
HammZ <henryhamon@gmail.com>
2017-05-15 16:21:23 +0000

    generate image

--- qrcoded.go
+++ qrcoded.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"image"
+	"image/png"
+	"io"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello QR Code")
 
-	qrcode := GenerateQRCode("555-2368")
-	ioutil.WriteFile("qrcode.png", qrcode, 0644)
+	file, _ := os.Create("qrcode.png")
+	defer file.Close()
+
+	GenerateQRCode(file, "555-2368")
 }
 
-func GenerateQRCode(code string) []byte {
-	return []byte{0xFF}
+func GenerateQRCode(w io.Writer, code string) {
+	img := image.NewRGBA(image.Rect(0, 0, 21, 21))
+	_ = png.Encode(w, img)
 }
--- qrcoded_test.go
+++ qrcoded_test.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"bytes"
+	"image/png"
 	"testing"
 )
 
 func TestGenerateQRCodeReturnsValue(t *testing.T) {
-	result := GenerateQRCode("555-2368")
+	buffer := new(bytes.Buffer)
+	GenerateQRCode(buffer, "555-2368")
 
-	if result == nil {
-		t.Errorf("Generated QRCode is nil")
+	if buffer.Len() == 0 {
+		t.Errorf("No QRCode generated")
+	}
+}
+
+func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
+	buffer := new(bytes.Buffer)
+	GenerateQRCode(buffer, "555-2368")
+
+	if buffer.Len() == 0 {
+		t.Errorf("No QRCode generated")
 	}
 
-	if len(result) == 0 {
-		t.Errorf("Generated QRCode has no data")
+	_, err := png.Decode(buffer)
+
+	if err != nil {
+		t.Errorf("Generated QRCode is not a PNG: %s", err)
 	}
 }
